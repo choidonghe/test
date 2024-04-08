02bde35fc384e6184d79fd08ddef88f1c06b7ac2
choidonghe <fsfsfs1@naver.com>
2024-04-08 16:44:06 +0900

    /test/기본설정 db테스트 완료

---
+++ test/src/test/java/test/itwillbs/controller/DBTest.java
@@ -0,0 +1,34 @@
+package test.itwillbs.controller;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(
+		locations = {"file:src/main/webapp/WEB-INF/spring/root-context.xml"}
+		)
+public class DBTest {
+
+	private static final Logger logger = LoggerFactory.getLogger(DBTest.class);
+	
+	@Inject
+	private SqlSession sqlSession;
+	
+	
+	@Test
+	public void dbtest(){
+		logger.info("dbtest()실행");
+		
+		logger.info("sqlSession=>"+sqlSession);
+		
+	}
+	
+}
